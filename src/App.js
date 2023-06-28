e1af5c1798d0b7f8a5416a1477c1018cbfe66853
Anmol Kumar Sharma <114172072+Akshar2363@users.noreply.github.com>
2023-06-28 21:22:36 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Timeline from './components/Timeline';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Navbar/>
+      <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/timeline" element={<Timeline/>}/>
+          <Route exact path="/skills" element={<Skills/>}/>
+          <Route exact path="/projects" element={<Projects/>}/>
+          <Route exact path="/contact" element={<Contact/>}/>
+        </Routes>
+    </Router>
     </div>
   );
 }
