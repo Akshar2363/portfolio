2f2b9448efa610eb36f8a1da77c44d6a8329214f
Anmol Kumar Sharma <114172072+Akshar2363@users.noreply.github.com>
2023-07-15 17:11:53 +0530

    Revert to previous branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,26 +45,26 @@ const Navbar = () => {
           <div className="navbar-list w-full h-full hidden md:flex text-center shadow-xl">
             <ul className='flex flex-col h-full justify-evenly'>
               <div className={`navbar-list-item border-b h-full flex items-center justify-center border-slate-500 text-lg p-3`}><Link to="/"><img src={logo} alt="" /></Link></div>
-              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/portfolio/home"><li className='px-5'> <FontAwesomeIcon icon={faHome}/> Home</li></Link>
-              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/about'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/portfolio/about"><li className='px-5'> <FontAwesomeIcon icon={faCircleInfo}/> About</li></Link>
-              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/timeline'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/portfolio/timeline"><li className='px-5'><FontAwesomeIcon icon={faTimeline}/> Timeline</li></Link>
-              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/skills'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/portfolio/skills"><li className='px-5'><FontAwesomeIcon icon={faLaptop} /> Skills</li></Link>
-              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/projects'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/portfolio/projects"><li className='px-5'><FontAwesomeIcon icon={faLaptopCode} /> Projects</li></Link>
-              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/contact'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/portfolio/contact"><li className='px-5'><FontAwesomeIcon icon={faContactBook} /> Contact</li></Link>
+              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/"><li className='px-5'> <FontAwesomeIcon icon={faHome}/> Home</li></Link>
+              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/about'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/about"><li className='px-5'> <FontAwesomeIcon icon={faCircleInfo}/> About</li></Link>
+              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/timeline'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/timeline"><li className='px-5'><FontAwesomeIcon icon={faTimeline}/> Timeline</li></Link>
+              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/skills'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/skills"><li className='px-5'><FontAwesomeIcon icon={faLaptop} /> Skills</li></Link>
+              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/projects'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/projects"><li className='px-5'><FontAwesomeIcon icon={faLaptopCode} /> Projects</li></Link>
+              <Link className={`flex justify-between w-full items-center navbar-list-item ${location.pathname==='/contact'? "bg-slate-600" : ""} text-white hover:bg-slate-700 border-b h-full  border-slate-500 text-xl`} to="/contact"><li className='px-5'><FontAwesomeIcon icon={faContactBook} /> Contact</li></Link>
             </ul>
           </div>
         </div>
         <div id="navbar-items" onClick={toggleMenu} className="z-10 navbar-items bg-slate-500 hidden flex items-center justify-center w-full text-center">
               <ul className='text-white text-xl md:text-3xl w-full h-fit grid grid-cols-2 justify-items-center'>
                 <div className='w-full grid grid-col justify-items-center'>
-                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/'? "bg-slate-400" : ""}`} to="/portfolio/home"><li><FontAwesomeIcon icon={faHome}/> Home</li></Link>
-                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/about'? "bg-slate-400" : ""}`} to="/portfolio/about"><li><FontAwesomeIcon icon={faCircleInfo}/> About</li></Link>
-                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/timeline'? "bg-slate-400" : ""}`} to="/portfolio/timeline"><li><FontAwesomeIcon icon={faTimeline}/> Timeline</li></Link>
+                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/'? "bg-slate-400" : ""}`} to="/"><li><FontAwesomeIcon icon={faHome}/> Home</li></Link>
+                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/about'? "bg-slate-400" : ""}`} to="/about"><li><FontAwesomeIcon icon={faCircleInfo}/> About</li></Link>
+                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/timeline'? "bg-slate-400" : ""}`} to="/timeline"><li><FontAwesomeIcon icon={faTimeline}/> Timeline</li></Link>
                 </div>
                 <div className='w-full grid grid-col justify-items-center'>
-                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/skills'? "bg-slate-400" : ""}`} to="/portfolio/skills"><li><FontAwesomeIcon icon={faLaptop}/> Skills</li></Link>
-                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/projects'? "bg-slate-400" : ""}`} to="/portfolio/projects"><li><FontAwesomeIcon icon={faLaptopCode}/> Projects</li></Link>
-                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/contact'? "bg-slate-400" : ""}`} to="/portfolio/contact"><li><FontAwesomeIcon icon={faContactBook}/> Contact</li></Link>
+                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/skills'? "bg-slate-400" : ""}`} to="/skills"><li><FontAwesomeIcon icon={faLaptop}/> Skills</li></Link>
+                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/projects'? "bg-slate-400" : ""}`} to="/projects"><li><FontAwesomeIcon icon={faLaptopCode}/> Projects</li></Link>
+                  <Link className={`p-2 m-2 hover:bg-slate-400 text-left px-2 rounded-xl w-[80%] text-sm md:text-lg ${location.pathname==='/contact'? "bg-slate-400" : ""}`} to="/contact"><li><FontAwesomeIcon icon={faContactBook}/> Contact</li></Link>
                 </div>
               </ul>
           </div>
