4c89a4c9edc400ed37ceeaefb4c504137688d93d
Anmol Kumar Sharma <114172072+Akshar2363@users.noreply.github.com>
2023-10-19 13:34:15 +0530

    Updated contact script URL

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,6 @@ import {Link} from 'react-router-dom';
 
 const About = () => {
 
-
     return (
         <div className="w-full mt-[80px] ml-0 md:ml-[200px] md:mt-0">
             <TsParticles/>
@@ -33,7 +32,6 @@ const About = () => {
                 </div>
 
             </div>
-
 
             <div className='text-white px-4 md:p-2 md:mx-8 flex flex-col items-center justify-between'>
 
